22765614f2133f2de6512c2d426ddfa71cd8ec11
ha1234ha <hasimoto.kengo@gmail.com>
2020-07-04 16:24:13 +0900

    fw追加

---
+++ iOS/testFw/testFw/testFw.h
@@ -0,0 +1,19 @@
+//
+//  testFw.h
+//  testFw
+//
+//  Created by oo on 2020/07/04.
+//  Copyright © 2020 testApp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testFw.
+FOUNDATION_EXPORT double testFwVersionNumber;
+
+//! Project version string for testFw.
+FOUNDATION_EXPORT const unsigned char testFwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testFw/PublicHeader.h>
+
+
